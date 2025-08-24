657d9e11e2c9a9ccf8c6ba68f40fddb0aa8bedf7
Halil Bilgin <bilginhalil@gmail.com>
2025-08-24 16:36:52 +0100

    More change

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -5,7 +5,7 @@ from typing import Iterable, Iterator
 
 from env_variables import NUM_PROCESSES
 from tqdm import tqdm
-from .tokenizer_utils import pretokenize, initialize_pair_frequencies, apply_merge, PAT, pretokenize_corpus_parallel
+from .tokenizer_utils import initialize_pair_frequencies, apply_merge, PAT, pretokenize_corpus_parallel
 import regex as re
 
 
